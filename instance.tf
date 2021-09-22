bb9b4d3678f978523cd97c4cf43081eb1843df3b
Sceva Jordan <sceva@Scevas-Air.attlocal.net>
2021-09-22 00:30:08 -0400

    updated with template file

--- instance.tf
+++ instance.tf
@@ -4,14 +4,14 @@ resource "aws_instance" "workstation" {
   #for_each      = data.aws_subnet_ids.selected.ids
 
   # the VPC subnet
-  subnet_id = tolist(data.aws_subnet_ids.${var.subnet}.ids)[0]
+  subnet_id = tolist(data.aws_subnet_ids.var.subnet.ids)[0]
 
   # the security group
   vpc_security_group_ids = [aws_security_group.allow-ssh.id]
 
   # the public SSH key
   key_name = aws_key_pair.mykeypair.key_name
-  user_data = "${file("install_ubuntu_docker.sh")}"
+  user_data = local.template_file_int
 
   tags = {
     Name = "workstation"
@@ -41,3 +41,8 @@ data "aws_subnet_ids" "public" {
     Name = "Demo-public-1"
   }
 }
+
+locals {
+   template_file_int  = templatefile("./docker_ubuntu_install.tpl")
+}
+
